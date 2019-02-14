16fb9ac586789fa23912c99e8affac1ebce4497b
Raul Madis Edovald <raedov@ttu.ee>
2019-02-14 21:20:49 +0200

    c

---
+++ ObjectLifetime/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Make = "Audi";
+            myCar.Model = "A3";
+            myCar.Year = 2009;
+            myCar.Color = "Dark Green";
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+ 
+            Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+            myOtherCar.Model = "A6";
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+
+            myOtherCar = null;
+
+            //Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+
+            myCar = null;
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
