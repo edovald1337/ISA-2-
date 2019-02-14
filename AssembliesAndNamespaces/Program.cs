70a05bbf165754cb824ebf730230ad82a6790e31
Raul Madis Edovald <raedov@ttu.ee>
2019-02-14 23:20:31 +0200

    c

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
--- UnderstandingScope/Program.cs
+++ UnderstandingScope/Program.cs
@@ -29,13 +29,33 @@ namespace UnderstandingScope
             //Console.WriteLine(i);
             Console.WriteLine("Outside of the for: " + j);
             Console.WriteLine("Outside of the for: " + k);
+
+
             HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+
+
             Console.ReadLine();
         }
 
         static void HelperMethod()
         {
             Console.WriteLine("Value of k from the HelperMethod(): " + k);
+        }
+    }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "Hello world!";
         }
     }
 }
