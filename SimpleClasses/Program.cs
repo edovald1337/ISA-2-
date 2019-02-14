3643baa22838a531b51dbb9082eba59abd3cc3db
Raul Madis Edovald <raedov@ttu.ee>
2019-02-14 21:06:20 +0200

    c

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -13,11 +13,24 @@ namespace SimpleClasses
             Car myCar = new Car();
             myCar.Make = "Audi";
             myCar.Model = "A3";
-            myCar.Year = "2009";
+            myCar.Year = 2009;
             myCar.Color = "Dark Green";
 
             Console.WriteLine("{0} {1} {2} {3}", myCar.Make,myCar.Model,myCar.Year,myCar.Color);
+
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}",value);
+
+            Console.WriteLine("{0:C}",myCar.DetermineMarketValue());
+
+
             Console.ReadLine();
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
         }
     }
 
@@ -25,7 +38,22 @@ namespace SimpleClasses
     {
         public string Make { get; set; }
         public string Model  { get; set; }
-        public string Year { get; set; }
+        public int Year { get; set; }
         public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+            {
+                carValue = 10000;
+            }
+            else
+            {
+                carValue = 2000;
+            }
+
+            return carValue;
+        }
     }
 }
